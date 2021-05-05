3abf07be0e208d99fe9bc3ae6a5e4511b22fe0b5
PatrickBoynton <jpboynton3@gmail.com>
2021-05-05 18:51:11 -0400

    Initial Commit.

---
+++ API/Program.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+           IHost host =  CreateHostBuilder(args).Build();
+
+           using IServiceScope scope = host.Services.CreateScope();
+
+           IServiceProvider services = scope.ServiceProvider;
+
+           try
+           {
+               DataContext context = services.GetRequiredService<DataContext>();
+               await context.Database.MigrateAsync();
+               await Seed.SeedData(context);
+           }
+           catch (Exception e)
+           {
+               ILogger logger = services.GetRequiredService<ILogger<Program>>();
+               
+               logger.LogError(e, "An error occured during migration.");
+           }
+           
+           host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
