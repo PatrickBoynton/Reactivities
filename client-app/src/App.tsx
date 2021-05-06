49c0d640aa25b6dc26ad73a1f70b3a3641271b9f
PatrickBoynton <jpboynton3@gmail.com>
2021-05-06 16:32:58 -0400

    Initial client commit.

---
+++ client-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios, { AxiosResponse } from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/v1/activities/').then((response: AxiosResponse) => {
+            console.log(response);
+            setActivities(response.data);
+        });
+    }, []);
+
+    return (
+        <>
+            <Header as='h2' icon='users' content='Reactivities'/>
+            <List>
+                { activities.map((activity: any) => (
+                    <List.Item key={ activity.id }>
+                        { activity.title }
+                    </List.Item>
+                )) }
+            </List>
+        </>
+    );
+}
+
+export default App;
