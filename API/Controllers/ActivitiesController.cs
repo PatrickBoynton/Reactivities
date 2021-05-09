5e44853412d9468a123da28c2b3481a60d48129b
PatrickBoynton <jpboynton3@gmail.com>
2021-05-09 17:47:54 -0400

    Added create and edit endpoints.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application;
 using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -10,12 +11,22 @@ namespace API.Controllers
     // http://localhost:5000/api/v1/activities/
     public class ActivitiesController : BaseApiController
     {
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities() => await Mediator.Send(new List.Query());
 
         [HttpGet("{id:guid}")]
         public async Task<Task<Activity>> GetActivity(Guid id) =>
-            Mediator.Send(new Details.Query{Id = id});
+            Mediator.Send(new Details.Query {Id = id});
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity) =>
+            Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateActivities(Guid id, Activity activity)
+        {
+            activity.Id = activity.Id;
+            return Ok(Mediator.Send(new Edit.Command {Activity = activity}));
+        }
     }
 }
