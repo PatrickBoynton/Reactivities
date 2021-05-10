534dd48de30788c5cbb7d3f85c563dbd13c426e7
PatrickBoynton <jpboynton3@gmail.com>
2021-05-10 01:16:12 -0400

    Changed the file structure a bit.

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './layout/index.css';
 import 'semantic-ui-css/semantic.css';
-import App from './App';
+import App from './layout/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
--- client-app/src/layout/App.tsx
+++ client-app/src/layout/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios, { AxiosResponse } from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
