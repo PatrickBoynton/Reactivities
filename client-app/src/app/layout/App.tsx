5817091e01ea195cfd0d62b37204c789d235aacd
PatrickBoynton <jpboynton3@gmail.com>
2021-05-10 12:07:13 -0400

    Added typescript.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-    const [activities, setActivities] = useState([]);
+    const [activities, setActivities] = useState<Activity[]>([]);
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/v1/activities/').then((response: AxiosResponse) => {
+        axios.get<Activity[]>('http://localhost:5000/api/v1/activities/').then((response: AxiosResponse) => {
             console.log(response);
             setActivities(response.data);
         });
@@ -16,7 +17,7 @@ function App() {
         <>
             <Header as='h2' icon='users' content='Reactivities'/>
             <List>
-                { activities.map((activity: any) => (
+                { activities.map(activity => (
                     <List.Item key={ activity.id }>
                         { activity.title }
                     </List.Item>
