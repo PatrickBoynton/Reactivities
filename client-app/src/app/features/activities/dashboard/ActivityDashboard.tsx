3c31adccfe6664c4254193c006b513f3abfdb33c
PatrickBoynton <jpboynton3@gmail.com>
2021-05-10 21:32:04 -0400

    Added an activity details section.

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../models/activity';
 import ActivityList from './ActivityList';
+import ActivityDetails from '../details/ActivityDetails';
 
 interface Props {
     activities: Activity[];
@@ -12,6 +13,10 @@ function ActivityDashboard({activities}: Props): ReactElement {
         <Grid>
             <Grid.Column width="10">
                 <ActivityList activities={activities} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] &&
+                <ActivityDetails activity={activities[0]} /> }
             </Grid.Column>
         </Grid>
     );
