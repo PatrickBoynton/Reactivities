60e992dda2d519405ba0150771a9c12b24f11c15
PatrickBoynton <jpboynton3@gmail.com>
2021-05-12 12:09:52 -0400

    Added the ability to type and submit the form data.

--- client-app/src/app/features/activities/form/ActivityForm.tsx
+++ client-app/src/app/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../models/activity';
 
@@ -7,18 +7,60 @@ interface Props {
     closeForm: () => void;
 }
 
-function ActivityForm({activity, closeForm}: Props): ReactElement {
+function ActivityForm({activity: selectedActivity, closeForm}: Props): ReactElement {
+
+    const initialState = selectedActivity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: '',
+    };
+
+    const [activity, setActivity] = useState(initialState);
+
+
+    const handleSubmit = (): void => {
+        console.log(activity);
+    };
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        const {name, value} = e.target;
+
+        setActivity({...activity, [name]: value});
+    };
+
     return (
         <Segment clearing>
-            <Form>
-                <Form.Input placeholder="Title"/>
-                <Form.TextArea placeholder="Description"/>
-                <Form.Input placeholder="Category"/>
-                <Form.Input placeholder="Date"/>
-                <Form.Input placeholder="City"/>
-                <Form.Input placeholder="Venue"/>
-                <Button basic positive floated='right' type='submit' content='Create Activity'/>
-                <Button onClick={closeForm} basic color='grey' floated='right' type='button' content='Cancel'/>
+            <Form onSubmit={ handleSubmit } autoComplete="off">
+                <Form.Input placeholder="Title"
+                            value={ activity.title }
+                            name="title"
+                            onChange={ handleInput }/>
+                <Form.TextArea placeholder="Description"
+                               value={ activity.description }
+                               name="description"
+                               onChange={ handleInput }/>
+                <Form.Input placeholder="Category"
+                            value={activity.category}
+                            name="category"
+                            onChange={handleInput}/>
+                <Form.Input placeholder="Date"
+                            value={activity.date}
+                            name="date"
+                            onChange={handleInput}/>
+                <Form.Input placeholder="City"
+                            value={activity.city}
+                            name="city"
+                            onChange={handleInput}/>
+                <Form.Input placeholder="Venue"
+                            value={activity.venue}
+                            name="venue"
+                            onChange={handleInput}/>
+                <Button basic positive floated="right" type="submit" content="Submit"/>
+                <Button onClick={ closeForm } basic color="grey" floated="right" type="button" content="Cancel"/>
             </Form>
         </Segment>
     );
