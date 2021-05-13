3e54de33cee50f05dbab917273eb2632214732e3
PatrickBoynton <jpboynton3@gmail.com>
2021-05-13 12:50:02 -0400

    Added the logic to get activities to the store.

---
+++ client-app/src/app/stores/activityStore.ts
@@ -0,0 +1,39 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+import { Activity } from '../models/activity';
+import agent from '../api/agent';
+
+export default class ActivityStore {
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    loadActivities = async () => {
+        this.setLoadingInitial(true);
+
+        try {
+            const activities = await agent.Activities.list();
+            runInAction(() => {
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activities.push(activity);
+                });
+                this.setLoadingInitial(false);
+            });
+        } catch (e) {
+            console.log(e);
+            runInAction(() => {
+                this.setLoadingInitial(false);
+            });
+        }
+    };
+
+    setLoadingInitial = (state: boolean): void => {
+        this.loadingInitial = state;
+    };
+}
