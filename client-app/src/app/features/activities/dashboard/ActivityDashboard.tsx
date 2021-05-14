690c9e08b3009b4e6976241b41bdd5d5945f2873
PatrickBoynton <jpboynton3@gmail.com>
2021-05-13 20:49:19 -0400

    More refactoring.

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,15 +4,11 @@ import { Activity } from '../../../models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
+import { useStore } from '../../../stores/store';
+import { observer } from 'mobx-react-lite';
 
 interface Props {
     activities: Activity[];
-    selectedActivity: Activity | undefined;
-    handleSelectActivity: (id: string) => void;
-    handleCancelActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
     editOrCreate: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
@@ -20,35 +16,28 @@ interface Props {
 
 function ActivityDashboard({
                                activities,
-                               selectedActivity,
-                               handleSelectActivity,
-                               handleCancelActivity,
-                               editMode,
-                               openForm,
-                               closeForm,
                                editOrCreate,
                                deleteActivity,
                                submitting,
                            }: Props): ReactElement {
+
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
+
     return (
         <Grid>
             <Grid.Column width="10">
                 <ActivityList activities={ activities }
-                              handleSelectActivity={ handleSelectActivity }
                               deleteActivity={ deleteActivity }
                               submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width="6">
                 { selectedActivity && !editMode &&
-                <ActivityDetails activity={ selectedActivity }
-                                 handleCancelActivity={ handleCancelActivity }
-                                 openForm={ openForm }/> }
+                <ActivityDetails /> }
 
                 { editMode &&
-                <ActivityForm closeForm={ closeForm }
-                              activity={ selectedActivity }
-                              editOrCreate={ editOrCreate }
+                <ActivityForm editOrCreate={ editOrCreate }
                               submitting={submitting}
                 />
                 }
@@ -57,4 +46,4 @@ function ActivityDashboard({
     );
 }
 
-export default ActivityDashboard;
+export default observer(ActivityDashboard);
