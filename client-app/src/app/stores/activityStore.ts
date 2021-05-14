13409ca4f551821dd264bf6e8b2f94da1da9e742
PatrickBoynton <jpboynton3@gmail.com>
2021-05-14 17:52:02 -0400

    Refactored to use Map, and added a sort date method.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,25 +4,23 @@ import agent from '../api/agent';
 import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
-    activities: Activity[] = [];
+    activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = false;
+    loadingInitial = true;
 
     constructor() {
         makeAutoObservable(this);
     }
 
     loadActivities = async () => {
-        this.setLoadingInitial(true);
-
         try {
             const activities = await agent.Activities.list();
             runInAction(() => {
                 activities.forEach(activity => {
                     activity.date = activity.date.split('T')[0];
-                    this.activities.push(activity);
+                    this.activityRegistry.set(activity.id, activity);
                 });
                 this.setLoadingInitial(false);
             });
@@ -39,7 +37,7 @@ export default class ActivityStore {
     };
 
     selectActivity = (id: string): void => {
-        this.selectedActivity = this.activities.find(a => a.id === id);
+        this.selectedActivity = this.activityRegistry.get(id)
     };
 
     cancelSelectedActivity = (): void => {
@@ -62,7 +60,7 @@ export default class ActivityStore {
         try {
             await agent.Activities.create(activity);
             runInAction(() => {
-                this.activities.push(activity);
+                this.activityRegistry.set(activity.id, activity);
                 this.selectedActivity = activity;
                 this.editMode = false;
                 this.loading = false;
@@ -82,7 +80,7 @@ export default class ActivityStore {
         try {
             await agent.Activities.update(activity);
             runInAction(() => {
-                this.activities = [...this.activities.filter(a => a.id !== activity.id), activity];
+                this.activityRegistry.set(activity.id, activity);
                 this.selectedActivity = activity;
                 this.editMode = false;
                 this.loading = false;
@@ -101,7 +99,7 @@ export default class ActivityStore {
         try {
             await agent.Activities.delete(id);
             runInAction(() => {
-                this.activities = [...this.activities.filter(a => a.id !== id)];
+                this.activityRegistry.delete(id);
                 if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             });
@@ -112,4 +110,9 @@ export default class ActivityStore {
             });
         }
     };
+
+    get activitiesByDate(): Activity[] {
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date));
+    }
 }
