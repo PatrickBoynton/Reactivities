8032bf7107e87421f21f8018c457e4a56dc94683
PatrickBoynton <jpboynton3@gmail.com>
2021-05-14 16:15:43 -0400

    Added a create and edit method.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 import { Activity } from '../models/activity';
 import agent from '../api/agent';
+import { v4 as uuid } from 'uuid';
 
 export default class ActivityStore {
     activities: Activity[] = [];
@@ -38,19 +39,59 @@ export default class ActivityStore {
     };
 
     selectActivity = (id: string): void => {
-        this.selectedActivity = this.activities.find(a => a.id === id)
-    }
+        this.selectedActivity = this.activities.find(a => a.id === id);
+    };
 
     cancelSelectedActivity = (): void => {
         this.selectedActivity = undefined;
-    }
+    };
 
     openForm = (id?: string): void => {
         id ? this.selectActivity(id) : this.cancelSelectedActivity();
         this.editMode = true;
-    }
+    };
 
     closeForm = (): void => {
         this.editMode = false;
-    }
+    };
+
+    createActivity = async (activity: Activity): Promise<void> => {
+        this.loading = true;
+        activity.id = uuid();
+
+        try {
+            await agent.Activities.create(activity);
+            runInAction(() => {
+                this.activities.push(activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            });
+        } catch (e) {
+            console.log(e);
+            runInAction(() => {
+                this.loading = false;
+            });
+        }
+    };
+
+
+    updateActivity = async (activity: Activity): Promise<void> => {
+        this.loading = true;
+
+        try {
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                this.activities = [...this.activities.filter(a => a.id !== activity.id), activity];
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            });
+        } catch (e) {
+            console.log(e);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    };
 }
