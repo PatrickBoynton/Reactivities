e73ebcdc882715034ca8af98bcc22e2d1d166f76
PatrickBoynton <jpboynton3@gmail.com>
2021-05-14 22:46:18 -0400

    Moved the logic from App to here.

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,22 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../layout/LoadingComponent';
 
 function ActivityDashboard(): ReactElement {
 
     const {activityStore} = useStore();
     const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]);
+
+    if (activityStore.loadingInitial) return <LoadingComponent content="Loading app"/>;
 
     return (
         <Grid>
