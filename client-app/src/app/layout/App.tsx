a548aa73992af145e324b5e163b84e659ef0df26
PatrickBoynton <jpboynton3@gmail.com>
2021-05-14 22:46:02 -0400

    Refactoring plus added routes.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,21 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import  { Route } from 'react-router-dom';
+import HomePage from '../features/home/HomePage';
+import ActivityForm from '../features/activities/form/ActivityForm';
 
 function App(): ReactElement {
-    const {activityStore} = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
-
-    if (activityStore.loadingInitial) return <LoadingComponent content="Loading app"/>;
 
     return (
         <>
             <NavBar />
             <Container style={ {marginTop: '7em'} }>
-                <ActivityDashboard/>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/activities' component={ActivityDashboard}/>
+                <Route path='/create' component={ActivityForm}/>
             </Container>
         </>
     );
