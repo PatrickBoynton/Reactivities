8da6a0048fe5581abe88cb49b097e9f7a3e7e873
PatrickBoynton <jpboynton3@gmail.com>
2021-05-16 16:45:54 -0400

    loaded activities.

--- client-app/src/app/features/activities/details/ActivityDetails.tsx
+++ client-app/src/app/features/activities/details/ActivityDetails.tsx
@@ -1,37 +1,49 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import { Card, Image, Button } from 'semantic-ui-react';
 import { useStore } from '../../../stores/store';
+import { useParams } from 'react-router-dom';
 import LoadingComponent from '../../../layout/LoadingComponent';
+import { observer } from 'mobx-react-lite';
 
 
 function ActivityDetails(): ReactElement {
     const {activityStore} = useStore();
-    const {selectedActivity: activity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
 
-    if (!activity) return <LoadingComponent />;
+
+    useEffect(() => {
+        if (id) {
+            loadActivity(id).then();
+        }
+    }, [id, loadActivity])
+
+    // TODO figure out why refreshing the page makes activity disappear.
+    if (loadingInitial || !activity) return <LoadingComponent/>;
+
     return (
         <Card fluid>
-            <Image src={ `/assets/categoryImages/${ activity.category }.jpg` } alt="Category"/>
+            <Image src={ `/assets/categoryImages/${ activity?.category }.jpg` } alt="Category"/>
 
             <Card.Content>
                 <Card.Header>
-                    { activity.title }
+                    { activity?.title }
                 </Card.Header>
                 <Card.Meta>
-                    { activity.date }
+                    { activity?.date }
                 </Card.Meta>
                 <Card.Description>
-                    { activity.description }
+                    { activity?.description }
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths="2">
-                    <Button  basic color="blue" content="Edit"/>
-                    <Button  basic color="grey" content="Cancel"/>
+                    <Button basic color="blue" content="Edit"/>
+                    <Button basic color="grey" content="Cancel"/>
                 </Button.Group>
             </Card.Content>
         </Card>
     );
 }
 
-export default ActivityDetails;
+export default observer(ActivityDetails);
