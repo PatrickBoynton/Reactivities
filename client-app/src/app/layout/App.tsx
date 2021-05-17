c4b11f4b33f4d3715429791ac536310993a212d3
PatrickBoynton <jpboynton3@gmail.com>
2021-05-17 15:01:17 -0400

    Moved the home outside the navbar.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,7 +3,7 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import  { Route, useLocation } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../features/home/HomePage';
 import ActivityForm from '../features/activities/form/ActivityForm';
 import ActivityDetails from '../features/activities/details/ActivityDetails';
@@ -12,13 +12,20 @@ function App(): ReactElement {
     const location = useLocation();
     return (
         <>
-            <NavBar />
-            <Container style={ {marginTop: '7em'} }>
-                <Route exact path='/' component={HomePage}/>
-                <Route exact path='/activities' component={ActivityDashboard}/>
-                <Route path='/activities/:id' component={ActivityDetails}/>
-                <Route key={location.key} path={ ['/create', '/manage/:id'] } component={ActivityForm}/>
-            </Container>
+            <Route exact path="/" component={ HomePage }/>
+            <Route path={ '/(.+)' }
+                   render={ () => (
+                       <>
+                           <NavBar/>
+                           <Container style={ {marginTop: '7em'} }>
+                               <Route exact path="/activities" component={ ActivityDashboard }/>
+                               <Route path="/activities/:id" component={ ActivityDetails }/>
+                               <Route key={ location.key } path={ ['/create', '/manage/:id'] }
+                                      component={ ActivityForm }/>
+                           </Container>
+                       </>
+                   ) }
+            />
         </>
     );
 }
