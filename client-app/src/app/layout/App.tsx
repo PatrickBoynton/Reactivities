ab9eb24c2c5b04f71f9fdee6727e4151c239c767
PatrickBoynton <jpboynton3@gmail.com>
2021-05-22 23:12:21 -0400

    Added react toastify. And an error route.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,11 +7,14 @@ import { Route, useLocation } from 'react-router-dom';
 import HomePage from '../features/home/HomePage';
 import ActivityForm from '../features/activities/form/ActivityForm';
 import ActivityDetails from '../features/activities/details/ActivityDetails';
+import TestErrors from '../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 function App(): ReactElement {
     const location = useLocation();
     return (
         <>
+            <ToastContainer position="bottom-right" hideProgressBar/>
             <Route exact path="/" component={ HomePage }/>
             <Route path={ '/(.+)' }
                    render={ () => (
@@ -22,6 +25,7 @@ function App(): ReactElement {
                                <Route path="/activities/:id" component={ ActivityDetails }/>
                                <Route key={ location.key } path={ ['/create', '/manage/:id'] }
                                       component={ ActivityForm }/>
+                               <Route path="/errors" component={ TestErrors }/>
                            </Container>
                        </>
                    ) }
