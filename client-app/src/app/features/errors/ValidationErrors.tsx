ac6589afbc538f6c113b0a2ca39368f913cb3f71
PatrickBoynton <jpboynton3@gmail.com>
2021-05-24 14:43:07 -0400

    Added a component to handle displaying the list of errors.

---
+++ client-app/src/app/features/errors/ValidationErrors.tsx
@@ -0,0 +1,25 @@
+import React, { ReactElement } from 'react';
+import { Message } from 'semantic-ui-react';
+
+interface Props {
+    errors: string[] | null
+}
+
+
+function ValidationErrors({errors}: Props): ReactElement {
+    return (
+        <Message error>
+            { errors && (
+                <Message.List>
+                    { errors.map((error: string, index: number) => (
+                        <Message.Item key={ index }>
+                            { error }
+                        </Message.Item>
+                    )) }
+                </Message.List>
+            ) }
+        </Message>
+    );
+}
+
+export default ValidationErrors;
