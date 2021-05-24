34cb79b953f92f06c3f6899efd8bbc12410f738d
PatrickBoynton <jpboynton3@gmail.com>
2021-05-24 14:42:22 -0400

    Updated the error test to show the validation errors on the page.

--- client-app/src/app/features/errors/TestError.tsx
+++ client-app/src/app/features/errors/TestError.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Button, Header, Segment} from "semantic-ui-react";
+import React, { useState } from 'react';
+import { Button, Header, Segment } from 'semantic-ui-react';
 import axios from 'axios';
+import ValidationErrors from './ValidationErrors';
 
 function TestErrors() {
-    const baseUrl = 'http://localhost:5000/api/v1/'
+    const baseUrl = 'http://localhost:5000/api/v1/';
+    const [errors, setErrors] = useState(null);
 
     function handleNotFound() {
         axios.get(baseUrl + 'buggy/not-found').catch(err => console.log(err.response));
@@ -26,24 +28,27 @@ function TestErrors() {
     }
 
     function handleValidationError() {
-        axios.post(baseUrl + 'activities', {}).catch(err => console.log(err.response));
+        axios.post(baseUrl + 'activities', {}).catch(err => setErrors(err));
     }
 
     return (
         <>
-            <Header as='h1' content='Test Error component' />
+            <Header as="h1" content="Test Error component"/>
             <Segment>
-                <Button.Group widths='7'>
-                    <Button onClick={handleNotFound} content='Not Found' basic primary />
-                    <Button onClick={handleBadRequest} content='Bad Request' basic primary />
-                    <Button onClick={handleValidationError} content='Validation Error' basic primary />
-                    <Button onClick={handleServerError} content='Server Error' basic primary />
-                    <Button onClick={handleUnauthorised} content='Unauthorised' basic primary />
-                    <Button onClick={handleBadGuid} content='Bad Guid' basic primary />
+                <Button.Group widths="7">
+                    <Button onClick={ handleNotFound } content="Not Found" basic primary/>
+                    <Button onClick={ handleBadRequest } content="Bad Request" basic primary/>
+                    <Button onClick={ handleValidationError } content="Validation Error" basic primary/>
+                    <Button onClick={ handleServerError } content="Server Error" basic primary/>
+                    <Button onClick={ handleUnauthorised } content="Unauthorised" basic primary/>
+                    <Button onClick={ handleBadGuid } content="Bad Guid" basic primary/>
                 </Button.Group>
             </Segment>
+            {errors &&
+                <ValidationErrors errors={errors} />
+            }
         </>
-    )
+    );
 }
 
 export default TestErrors;
