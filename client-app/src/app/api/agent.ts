37a6c35bee4f2638919523dfb74fdb91685546f0
PatrickBoynton <jpboynton3@gmail.com>
2021-05-24 03:43:23 -0400

    Added a switch statement to handle errors.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { Activity } from '../models/activity';
+import { toast } from 'react-toastify';
 
 
 const sleep = (delay: number) => {
@@ -11,13 +12,26 @@ const sleep = (delay: number) => {
 axios.defaults.baseURL = 'http://localhost:5000/api/v1/';
 
 axios.interceptors.response.use(async response => {
-    try {
-        await sleep(1000);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return Promise.reject(error);
+    await sleep(1000);
+    return response;
+}, (error: AxiosError) => {
+    const {data, status} = error.response!;
+
+    switch (status) {
+        case 400:
+            toast.error('Bad request.');
+            break;
+        case 401:
+            toast.error('Unauthorized.');
+            break;
+        case 404:
+            toast.error('Not found.');
+            break;
+        case 500:
+            toast.error('Internal Server Error.');
+            break;
     }
+    return Promise.reject(error);
 });
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
@@ -31,10 +45,10 @@ const requests = {
 
 const Activities = {
     list: () => requests.get<Activity[]>('activities/'),
-    details: (id: string) => requests.get<Activity>(`activities/${id}`),
+    details: (id: string) => requests.get<Activity>(`activities/${ id }`),
     create: (activity: Activity) => requests.post('activities/', activity),
-    update: (activity: Activity) => requests.put(`activities/${activity.id}`, activity),
-    delete: (id: string) => requests.del(`activities/${id}`),
+    update: (activity: Activity) => requests.put(`activities/${ activity.id }`, activity),
+    delete: (id: string) => requests.del(`activities/${ id }`),
 };
 
 const agent = {
