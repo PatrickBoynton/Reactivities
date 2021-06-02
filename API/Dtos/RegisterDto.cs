e6f77555ff3d0e6c37da3d48ba58a4cd2a81229b
PatrickBoynton <jpboynton3@gmail.com>
2021-06-02 16:14:25 -0400

    Added validation.

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z]).{4, }$", ErrorMessage = "Password must be more complex.")]
         public string Password { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 }
