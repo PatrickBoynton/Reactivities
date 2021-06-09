5e5d0efc24b96dc4d49faf93bf2f4b89335bff5c
PatrickBoynton <jpboynton3@gmail.com>
2021-06-09 02:51:59 -0400

    Added the new login method.

--- client-app/src/app/features/users/LoginForm.tsx
+++ client-app/src/app/features/users/LoginForm.tsx
@@ -2,21 +2,32 @@ import { Form, Formik } from 'formik';
 import React from 'react';
 import TextInput from '../../common/form/TextInput';
 import { Button } from 'semantic-ui-react';
+import { useStore } from '../../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function LoginForm() {
+    const {userStore} = useStore();
     return (
-        <Formik initialValues={ {email: '', password: ''} } onSubmit={ (values) => console.log(values) }>
-            { ({handleSubmit}) =>
-                <Form className="ui form" onSubmit={ handleSubmit } autoComplete="off">
+        <Formik initialValues={ {email: '', password: ''} }
+                onSubmit={ (values) => userStore.login(values) }>
+            { ({handleSubmit, isSubmitting}) =>
+                <Form className="ui form"
+                      onSubmit={ handleSubmit }
+                      autoComplete="off">
+
                     <TextInput placeholder="Email" name="email"/>
                     <TextInput placeholder="Password"
                                name="password"
                                type="password"/>
 
-                    <Button positive content='Login' type='submit' fluid/>
+                    <Button loading={ isSubmitting }
+                            positive
+                            content="Login"
+                            type="submit"
+                            fluid/>
                 </Form> }
         </Formik>
     );
 }
 
-export default LoginForm;
+export default observer(LoginForm);
