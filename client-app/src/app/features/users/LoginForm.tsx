a34d146d15cc14a9b369fd938d72d5e97ae355de
PatrickBoynton <jpboynton3@gmail.com>
2021-06-10 02:55:02 -0400

    Added the ability to display errors on the form, and not just in a toast.

--- client-app/src/app/features/users/LoginForm.tsx
+++ client-app/src/app/features/users/LoginForm.tsx
@@ -1,20 +1,22 @@
-import { Form, Formik } from 'formik';
+import { ErrorMessage, Form, Formik } from 'formik';
 import React from 'react';
 import TextInput from '../../common/form/TextInput';
-import { Button } from 'semantic-ui-react';
+import { Button, Label } from 'semantic-ui-react';
 import { useStore } from '../../stores/store';
 import { observer } from 'mobx-react-lite';
 
 function LoginForm() {
     const {userStore} = useStore();
     return (
-        <Formik initialValues={ {email: '', password: ''} }
-                onSubmit={ (values) => userStore.login(values) }>
-            { ({handleSubmit, isSubmitting}) =>
+        <Formik initialValues={ {email: '', password: '', error: null} }
+                onSubmit={ (values, {setErrors}) => userStore.login(values)
+                    .catch(error => setErrors({error: 'Invalid email or password.'})) }>
+            { ({handleSubmit, isSubmitting, errors}) =>
                 <Form className="ui form"
                       onSubmit={ handleSubmit }
                       autoComplete="off">
-
+                    <ErrorMessage name="error" render={ () => <Label style={ {marginBottom: 10} } basic color="red"
+                                                                     content={errors.error }/> }/>
                     <TextInput placeholder="Email" name="email"/>
                     <TextInput placeholder="Password"
                                name="password"
