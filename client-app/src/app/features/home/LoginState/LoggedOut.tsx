862ecedaa50595730b01dac7df00277c1ca5c62c
PatrickBoynton <jpboynton3@gmail.com>
2021-06-13 01:35:39 -0400

    Changed the login to be a modal. Added a Register button.

--- client-app/src/app/features/home/LoginState/LoggedOut.tsx
+++ client-app/src/app/features/home/LoginState/LoggedOut.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { ReactElement } from 'react';
 import { Button } from 'semantic-ui-react';
+import ModalStore from '../../../stores/modalStore';
+import LoginForm from '../../users/LoginForm';
 
-const LoggedOut = () => {
+interface Props {
+    modalStore: ModalStore;
+}
+
+const LoggedOut = ({modalStore}: Props): ReactElement => {
     return (
-        <><Button as={ Link }
+        <><Button onClick={() => modalStore.openModal(<LoginForm/>)}
                   to="/login"
                   size="huge"
                   inverted
                   content="Login"/>
+            <Button onClick={() => modalStore.openModal(<h1>Register</h1>)}
+                    to="/login"
+                    size="huge"
+                    inverted
+                    content="Register"/>
         </>
     );
 };
