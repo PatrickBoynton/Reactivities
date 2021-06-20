10b403c0611d95adb5d7865639d4427926d0dc03
PatrickBoynton <jpboynton3@gmail.com>
2021-06-20 14:19:11 -0400

    Updated the create to add the current user as a host.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -27,13 +29,28 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
                 _context = context;
+                _userAccessor = userAccessor;
             }
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                AppUser user = await _context.Users
+                    .FirstOrDefaultAsync(u => u.UserName == _userAccessor.GetUsername());
+
+                ActivityAttendee attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true,
+                };
+                
+                request.Activity.Attendees.Add(attendee);
+
+
                 _context.Activities.Add(request.Activity);
 
                bool result = await _context.SaveChangesAsync() > 0;
