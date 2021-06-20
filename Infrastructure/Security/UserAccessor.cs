3437cf7018299158a305b173a54532e1182166d9
PatrickBoynton <jpboynton3@gmail.com>
2021-06-20 14:08:23 -0400

    Created a user accessor class.

---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,19 @@
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public UserAccessor(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+        
+        
+        public string GetUsername() => _contextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+    }
+}
