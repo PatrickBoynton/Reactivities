bc2c71c13c7010ed61480a708b8d1f261f3156bb
PatrickBoynton <jpboynton3@gmail.com>
2021-06-21 01:06:06 -0400

    Changed the name of the property.

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -14,6 +14,6 @@ namespace Application.Activities
         public string City { get; set; }
         public string Venue { get; set; }
         public string HostUsername { get; set; }
-        private ICollection<Profile> Profiles { get; set; }
+        private ICollection<Profile> Attendees { get; set; }
     }
 }
