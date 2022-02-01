37a6c585bbc4631045c622087f56f12584c52f4e
PatrickBoynton <jpboynton3@gmail.com>
2022-02-01 12:18:48 -0500

    Updated the program to create a database on program start.

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 namespace API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using IServiceScope scope = host.Services.CreateScope();
+
+            IServiceProvider services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                
+                context.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration.");
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
