ae32700b7cf3fe3c008fa22d5bc3879e4d23df47
PatrickBoynton <jpboynton3@gmail.com>
2022-02-13 12:07:00 -0500

    Created the activity details component.

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,34 @@
+import { Button, Card, Image } from "semantic-ui-react";
+import { Activity } from "../../../app/models/Activity";
+
+interface Props {
+    activity: Activity;
+}
+
+const ActivityDetails = ({activity}: Props) => {
+    return <Card fluid>
+        <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
+        <Card.Content>
+            <Card.Header>
+                {activity.title}
+            </Card.Header>
+            <Card.Meta>
+                    <span>
+                        {activity.date}
+                    </span>
+            </Card.Meta>
+            <Card.Description>
+                {activity.description}
+            </Card.Description>
+        </Card.Content>
+        <Card.Content>
+            <Button.Group widths='2'>
+                <Button basic color='blue' content='Edit'/>
+                <Button basic color='grey' content='Cancel'/>
+            </Button.Group>
+        </Card.Content>
+    </Card>
+}
+
+
+export default ActivityDetails;
