9c002e678516f48495c537e47aecdcc62683fb49
PatrickBoynton <jpboynton3@gmail.com>
2022-02-13 13:30:43 -0500

    Passed down the proper methods, and state.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -2,33 +2,34 @@ import { Button, Card, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
 
 interface Props {
-    activity: Activity;
+    activity: Activity | undefined;
+    cancelSelectActivity: () => void;
 }
 
-const ActivityDetails = ({activity}: Props) => {
+const ActivityDetails = ({activity, cancelSelectActivity}: Props) => {
     return <Card fluid>
-        <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
+        <Image src={`/assets/categoryImages/${activity?.category}.jpg`}/>
         <Card.Content>
             <Card.Header>
-                {activity.title}
+                {activity?.title}
             </Card.Header>
             <Card.Meta>
                     <span>
-                        {activity.date}
+                        {activity?.date}
                     </span>
             </Card.Meta>
             <Card.Description>
-                {activity.description}
+                {activity?.description}
             </Card.Description>
         </Card.Content>
         <Card.Content>
-            <Button.Group widths='2'>
-                <Button basic color='blue' content='Edit'/>
-                <Button basic color='grey' content='Cancel'/>
+            <Button.Group widths="2">
+                <Button basic color="blue" content="Edit"/>
+                <Button onClick={cancelSelectActivity} basic color="grey" content="Cancel"/>
             </Button.Group>
         </Card.Content>
-    </Card>
-}
+    </Card>;
+};
 
 
 export default ActivityDetails;
