7386ba960af43f86311b34f5b41c2065e2690057
PatrickBoynton <jpboynton3@gmail.com>
2022-02-21 14:19:04 -0500

    Used the deleteActivity function.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,9 +5,10 @@ import { Activity } from "../../../app/models/Activity";
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-const ActivityList = ({activities, selectActivity}: Props) => {
+const ActivityList = ({activities, selectActivity, deleteActivity}: Props) => {
     return <Segment>
         <Item.Group divided>
             {activities.map(activity => (
@@ -30,6 +31,8 @@ const ActivityList = ({activities, selectActivity}: Props) => {
                         <Item.Extra>
                             <Button onClick={() => selectActivity(activity.id)} floated="right" content="View"
                                     color="blue"/>
+                            <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete"
+                                    color="red"/>
                             <Label basic content={activity.category}/>
                         </Item.Extra>
                     </Item.Content>
