c007370899662241c30c4f8da4196a7a7e5e529b
PatrickBoynton <jpboynton3@gmail.com>
2022-02-26 15:28:57 -0500

    Added  delete functionality.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
 
@@ -6,9 +7,16 @@ interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-const ActivityList = ({activities, selectActivity, deleteActivity}: Props) => {
+const ActivityList = ({activities, selectActivity, deleteActivity, submitting}: Props) => {
+    const [target, setTarget] = useState("");
+
+    const handleDeleteActivity = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    };
     return <Segment>
         <Item.Group divided>
             {activities.map(activity => (
@@ -31,7 +39,11 @@ const ActivityList = ({activities, selectActivity, deleteActivity}: Props) => {
                         <Item.Extra>
                             <Button onClick={() => selectActivity(activity.id)} floated="right" content="View"
                                     color="blue"/>
-                            <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete"
+                            <Button name={activity.id}
+                                    loading={submitting && target === activity.id}
+                                    onClick={(e) => handleDeleteActivity(e, activity.id)}
+                                    floated="right"
+                                    content="Delete"
                                     color="red"/>
                             <Label basic content={activity.category}/>
                         </Item.Extra>
