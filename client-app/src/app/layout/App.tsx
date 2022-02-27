c973591581d7842b868d005736bda0441d19c281
PatrickBoynton <jpboynton3@gmail.com>
2022-02-27 14:14:04 -0500

    Removed unused code.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,17 @@
 import { observer } from "mobx-react-lite";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import agent from "../api/agent";
-import { Activity } from "../models/Activity";
 import { useStore } from "../stores/store";
 import LoadingComponent from "./LoadingComponents";
 import Navbar from "./Navbar";
 
 const App = () => {
     const {activityStore} = useStore();
-    const [activities, setActivities] = useState<Activity[]>([]);
-    const [submitting, setSubmitting] = useState(false);
 
     useEffect(() => {
         activityStore.loadActivities();
     }, [activityStore]);
-
-    const handleDeleteActivity = (id: string) => {
-        setSubmitting(true);
-        agent.Activities.delete(id).then(() => {
-            setActivities([...activities.filter(activity => activity.id !== id)]);
-            setSubmitting(false);
-        });
-    };
 
     if (activityStore.loadingInitial) return <LoadingComponent/>;
 
@@ -31,10 +19,7 @@ const App = () => {
         <>
             <Navbar/>
             <Container style={{marginTop: "7em"}}>
-                <ActivityDashboard activities={activityStore.activities}
-                                   deleteActivity={handleDeleteActivity}
-                                   submitting={submitting}
-                />
+                <ActivityDashboard />
             </Container>
         </>
     );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,24 +6,14 @@ import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
-interface Props {
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
-
-const ActivityDashboard = ({activities, deleteActivity, submitting}: Props) => {
+const ActivityDashboard = () => {
     
     const { activityStore } = useStore();
     const { selectedActivity, editMode } = activityStore;
 
     return <Grid>
         <Grid.Column width="10">
-            <ActivityList activities={activities}
-                          deleteActivity={deleteActivity}
-                          submitting={submitting}
-            />
+            <ActivityList  />
         </Grid.Column>
         <Grid.Column width="6">
             {selectedActivity && !editMode &&
