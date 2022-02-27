0f8b0be55db5863179621742988131f40d61f5da
PatrickBoynton <jpboynton3@gmail.com>
2022-02-27 13:34:25 -0500

    Added the store, and removed some props.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,26 @@
+import { observer } from "mobx-react-lite";
 import { SyntheticEvent, useState } from "react";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
+import { useStore } from "../../../app/stores/store";
 
 
 interface Props {
     activities: Activity[];
-    selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
-const ActivityList = ({activities, selectActivity, deleteActivity, submitting}: Props) => {
+const ActivityList = ({activities, deleteActivity, submitting}: Props) => {
     const [target, setTarget] = useState("");
 
     const handleDeleteActivity = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
         setTarget(e.currentTarget.name);
         deleteActivity(id);
     };
+
+    const {activityStore} = useStore();
+
     return <Segment>
         <Item.Group divided>
             {activities.map(activity => (
@@ -37,7 +41,9 @@ const ActivityList = ({activities, selectActivity, deleteActivity, submitting}: 
                             </div>
                         </Item.Description>
                         <Item.Extra>
-                            <Button onClick={() => selectActivity(activity.id)} floated="right" content="View"
+                            <Button onClick={() => activityStore.selectActivity(activity.id)} 
+                                    floated="right" 
+                                    content="View"
                                     color="blue"/>
                             <Button name={activity.id}
                                     loading={submitting && target === activity.id}
@@ -54,4 +60,4 @@ const ActivityList = ({activities, selectActivity, deleteActivity, submitting}: 
     </Segment>;
 };
 
-export default ActivityList;
+export default observer(ActivityList);
