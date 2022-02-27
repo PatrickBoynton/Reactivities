3319a8a8202fa946d4954d8151558371bbf41e9a
PatrickBoynton <jpboynton3@gmail.com>
2022-02-27 17:30:24 -0500

    Created a new sort function, and made minor formatting changes.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,28 +1,31 @@
 import { makeAutoObservable, runInAction } from "mobx";
+import { v4 as uuid } from "uuid";
 import agent from "../api/agent";
 import { Activity } from "../models/Activity";
-import { v4 as uuid } from "uuid";
 
 export default class ActivityStore {
-    activities: Activity[] = [];
+    activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = false;
+    loadingInitial = true;
 
 
     constructor() {
         makeAutoObservable(this);
     }
 
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
     loadActivities = async () => {
-        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
 
             activities.forEach(activity => {
                 activity.date = activity.date.split("T")[0];
-                this.activities.push(activity);
+                this.activityRegistry.set(activity.id, activity);
             });
             this.setLoadingInitial(false);
 
@@ -37,7 +40,7 @@ export default class ActivityStore {
     };
 
     selectActivity = (id: string) => {
-        this.selectedActivity = this.activities.find(activity => activity.id === id);
+        this.selectedActivity = this.activityRegistry.get(id);
     };
 
     cancelSelectedActivity = () => {
@@ -59,7 +62,7 @@ export default class ActivityStore {
         try {
             await agent.Activities.create(activity);
             runInAction(() => {
-                this.activities.push(activity);
+                this.activityRegistry.set(activity.id, activity);
                 this.selectedActivity = activity;
                 this.editMode = false;
             });
@@ -68,25 +71,25 @@ export default class ActivityStore {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
-            })
+            });
         }
     };
 
-     updateActivity = async (activity: Activity) => {
+    updateActivity = async (activity: Activity) => {
         this.loading = true;
         try {
             await agent.Activities.update(activity);
             runInAction(() => {
-                this.activities = [...this.activities.filter(a => a.id !== activity.id, activity)]
+                this.activityRegistry.set(activity.id, activity);
                 this.selectedActivity = activity;
                 this.editMode = false;
                 this.loading = false;
-            })
+            });
         } catch (error) {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
-            })
+            });
         }
     };
 
@@ -95,15 +98,15 @@ export default class ActivityStore {
         try {
             await agent.Activities.delete(id);
             runInAction(() => {
-                this.activities = [...this.activities.filter(a => a.id !== id)];
+                this.activityRegistry.delete(id);
                 if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
-            })
+            });
         } catch (error) {
             console.log(error);
             runInAction(() => {
                 this.loading = false;
-            })
+            });
         }
     };
 }
