65562c6b6be47d3577da4e87abdb15ae2b68f0ee
PatrickBoynton <jpboynton3@gmail.com>
2022-02-27 13:33:20 -0500

    Moved code related to activities, and forms here.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,7 +4,7 @@ import { Activity } from "../models/Activity";
 
 export default class ActivityStore {
     activities: Activity[] = [];
-    selectedActivity: Activity | null = null;
+    selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
     loadingInitial = false;
@@ -19,7 +19,7 @@ export default class ActivityStore {
         try {
             const activities = await agent.Activities.list();
 
-            activities.map(activity => {
+            activities.forEach(activity => {
                 activity.date = activity.date.split("T")[0];
                 this.activities.push(activity);
             });
@@ -34,4 +34,21 @@ export default class ActivityStore {
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     };
+
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activities.find(activity => activity.id === id);
+    };
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    };
+
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    };
+
+    closeForm = () => {
+        this.editMode = false;
+    }
 }
