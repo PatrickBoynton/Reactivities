b0556c3f92378d0c7541efe2c9624d7f8ff6904d
PatrickBoynton <jpboynton3@gmail.com>
2022-02-26 21:45:46 -0500

    Removed the agent call, added the store call.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,32 +1,24 @@
+import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
 import { Container } from "semantic-ui-react";
 import { v4 as uuid } from "uuid";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
 import { Activity } from "../models/Activity";
+import { useStore } from "../stores/store";
 import LoadingComponent from "./LoadingComponents";
 import Navbar from "./Navbar";
 
-function App() {
+const App = () => {
+    const {activityStore} = useStore();
     const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
-    const [loading, setLoading] = useState(true);
     const [submitting, setSubmitting] = useState(false);
 
     useEffect(() => {
-        agent.Activities.list().then(response => {
-            let activities: Activity[] = [];
-
-            response.forEach(activity => {
-                activity.date = activity.date.split("T")[0];
-                activities.push(activity);
-            });
-
-            setActivities(activities);
-            setLoading(false);
-        });
-    }, []);
+        activityStore.loadActivities();
+    }, [activityStore]);
 
     const handleSelectActivity = (id: string) => {
         setSelectedActivity(activities.find(activity => activity.id === id));
@@ -73,13 +65,13 @@ function App() {
         });
     };
 
-    if (loading) return <LoadingComponent/>;
+    if (activityStore.loadingInitial) return <LoadingComponent/>;
 
     return (
         <>
             <Navbar openForm={handleFormOpen}/>
             <Container style={{marginTop: "7em"}}>
-                <ActivityDashboard activities={activities}
+                <ActivityDashboard activities={activityStore.activities}
                                    selectedActivity={selectedActivity}
                                    selectActivity={handleSelectActivity}
                                    cancelSelectActivity={handleCancelActivity}
@@ -93,6 +85,6 @@ function App() {
             </Container>
         </>
     );
-}
+};
 
-export default App;
+export default observer(App);
