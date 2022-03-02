231e6ad5a9e3a9c91885bbe062f1e6a0e8f12b21
PatrickBoynton <jpboynton3@gmail.com>
2022-03-01 21:05:36 -0500

    Removed loading the activities from the main page.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,19 @@
 import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useStore } from "../stores/store";
-import LoadingComponent from "./LoadingComponents";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import HomePage from "../../features/home/HomePage";
 import Navbar from "./Navbar";
 
 const App = () => {
-    const {activityStore} = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
-
-    if (activityStore.loadingInitial) return <LoadingComponent/>;
-
     return (
         <>
             <Navbar/>
             <Container style={{marginTop: "7em"}}>
-                <ActivityDashboard />
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/activities" component={ActivityDashboard}/>
+                <Route path="/createActivity" component={ActivityForm}/>
             </Container>
         </>
     );
