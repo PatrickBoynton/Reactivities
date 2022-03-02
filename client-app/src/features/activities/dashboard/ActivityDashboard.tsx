190005c1cab001e293e60784d1937eb7ace9975e
PatrickBoynton <jpboynton3@gmail.com>
2022-03-01 21:05:59 -0500

    Moved the loading activities here.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,14 +1,22 @@
 import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 const ActivityDashboard = () => {
-
     const {activityStore} = useStore();
+
+    useEffect(() => {
+        activityStore.loadActivities();
+    }, [activityStore]);
+
     const {selectedActivity, editMode} = activityStore;
+
+    if (activityStore.loadingInitial) return <LoadingComponent/>;
 
     return <Grid>
         <Grid.Column width="10">
