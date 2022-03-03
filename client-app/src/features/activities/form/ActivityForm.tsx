8cc10e3330fc34981d938c8fea8fcbe839d113d1
PatrickBoynton <jpboynton3@gmail.com>
2022-03-02 21:14:55 -0500

    Added a check for individual activities by id.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,14 +1,17 @@
 import { observer } from "mobx-react-lite";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
 
 
 const ActivityForm = () => {
     const {activityStore} = useStore();
-    const {selectedActivity, createActivity, updateActivity, loading} = activityStore;
+    const {loadActivity, createActivity, updateActivity, loading, loadingInitial} = activityStore;
+    const {id} = useParams<{ id: string }>();
 
-    const initialState = selectedActivity ?? {
+    const [activity, setActivity] = useState({
         id: "",
         title: "",
         category: "",
@@ -16,9 +19,11 @@ const ActivityForm = () => {
         date: "",
         city: "",
         venue: ""
-    };
+    });
 
-    const [activity, setActivity] = useState(initialState);
+    useEffect(() => {
+        if (id) loadActivity(id).then(activity => setActivity(activity!));
+    }, [id, loadActivity]);
 
     const handleSubmit = () => {
         activity.id ? updateActivity(activity) : createActivity(activity);
@@ -31,13 +36,17 @@ const ActivityForm = () => {
     };
 
 
+    if (loadingInitial) return <LoadingComponent content="Loading..."/>;
+
     return <Segment clearing>
         <Form onSubmit={handleSubmit} autoComplete="off">
             <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange}/>
             <Form.TextArea placeholder="Description" value={activity.description} name="description"
                            onChange={handleInputChange}/>
-            <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange}/>
-            <Form.Input type="date" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange}/>
+            <Form.Input placeholder="Category" value={activity.category} name="category"
+                        onChange={handleInputChange}/>
+            <Form.Input type="date" placeholder="Date" value={activity.date} name="date"
+                        onChange={handleInputChange}/>
             <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange}/>
             <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange}/>
             <Button floated="right" positive type="submit" content="Submit"/>
