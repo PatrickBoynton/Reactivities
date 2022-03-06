adec11419704dd0fd5415e0fae835ebc8dd2179c
PatrickBoynton <jpboynton3@gmail.com>
2022-03-05 22:17:45 -0500

    Formatting and bug fixes.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,58 +1,90 @@
 import { observer } from "mobx-react-lite";
 import { ChangeEvent, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { Button, Form, Segment } from "semantic-ui-react";
+import { v4 as uuid } from "uuid";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { useStore } from "../../../app/stores/store";
 
 
 const ActivityForm = () => {
-    const {activityStore} = useStore();
-    const {loadActivity, createActivity, updateActivity, loading, loadingInitial} = activityStore;
-    const {id} = useParams<{ id: string }>();
+	const history = useHistory();
+	const {activityStore} = useStore();
+	const {loadActivity, createActivity, updateActivity, loading, loadingInitial} = activityStore;
+	const {id} = useParams<{ id: string }>();
+	const [activity, setActivity] = useState({
+		id: "",
+		title: "",
+		category: "",
+		description: "",
+		date: "",
+		city: "",
+		venue: ""
+	});
 
-    const [activity, setActivity] = useState({
-        id: "",
-        title: "",
-        category: "",
-        description: "",
-        date: "",
-        city: "",
-        venue: ""
-    });
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		if (id) loadActivity(id).then(activity => setActivity(activity!));
+	}, [id, loadActivity]);
 
-    useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!));
-    }, [id, loadActivity]);
-
-    const handleSubmit = () => {
-        activity.id ? updateActivity(activity) : createActivity(activity);
-    };
-
-
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const {name, value} = event.target;
-        setActivity({...activity, [name]: value});
-    };
+	const handleSubmit = () => {
+		if (activity.id.length === 0) {
+			const newActivity = {
+				...activity,
+				id: uuid()
+			};
+			createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+		} else {
+			updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+		}
+	};
 
 
-    if (loadingInitial) return <LoadingComponent content="Loading..."/>;
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		const {name, value} = event["target"];
+		setActivity({...activity, [name]: value});
+	};
 
-    return <Segment clearing>
-        <Form onSubmit={handleSubmit} autoComplete="off">
-            <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange}/>
-            <Form.TextArea placeholder="Description" value={activity.description} name="description"
-                           onChange={handleInputChange}/>
-            <Form.Input placeholder="Category" value={activity.category} name="category"
-                        onChange={handleInputChange}/>
-            <Form.Input type="date" placeholder="Date" value={activity.date} name="date"
-                        onChange={handleInputChange}/>
-            <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange}/>
-            <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange}/>
-            <Button floated="right" positive type="submit" content="Submit"/>
-            <Button loading={loading} floated="right" type="button" content="Cancel"/>
-        </Form>
-    </Segment>;
+
+	if (loadingInitial) return <LoadingComponent content="Loading..."/>;
+
+	return <Segment clearing>
+		<Form onSubmit={handleSubmit} autoComplete="off">
+			<Form.Input
+				placeholder="Title"
+				value={activity.title}
+				name="title"
+				onChange={handleInputChange}/>
+			<Form.TextArea
+				placeholder="Description"
+				value={activity.description}
+				name="description"
+				onChange={handleInputChange}/>
+			<Form.Input
+				placeholder="Category"
+				value={activity.category}
+				name="category"
+				onChange={handleInputChange}/>
+			<Form.Input
+				type="date"
+				placeholder="Date"
+				value={activity.date}
+				name="date"
+				onChange={handleInputChange}/>
+			<Form.Input
+				placeholder="City"
+				value={activity.city}
+				name="city"
+				onChange={handleInputChange}/>
+			<Form.Input
+				placeholder="Venue"
+				value={activity.venue}
+				name="venue"
+				onChange={handleInputChange}/>
+			<Button loading={loading} floated="right" positive type="submit" content="Submit"/>
+			<Button floated="right" type="button" content="Cancel"/>
+		</Form>
+	</Segment>;
 };
 
 export default observer(ActivityForm);
