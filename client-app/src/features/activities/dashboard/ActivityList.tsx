62bbd1a5acc2ec08b46a580e75bc089a72bbc476
PatrickBoynton <jpboynton3@gmail.com>
2022-03-19 15:12:51 -0400

    Restyled the activity details.

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,7 +2,7 @@
 
 import { observer } from "mobx-react-lite";
 import { Fragment } from "react";
-import { Header, Item, Segment } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import ActivityListItem from "./ActivityListItem";
 
@@ -13,17 +13,13 @@ const ActivityList = () => {
 
 	return <>
 		{groupedActivities.map(([group, activities]) => (
-			<Fragment key={group}>
-				<Header sub color="teal">
+			<Fragment key = {group}>
+				<Header sub color = "teal">
 					{group}
 				</Header>
-				<Segment>
-					<Item.Group>
-						{activities.map(activity => (
-							<ActivityListItem  key={activity.id} activity={activity} />
-						))}
-					</Item.Group>
-				</Segment>
+				{activities.map(activity => (
+					<ActivityListItem key = {activity.id} activity = {activity} />
+				))}
 			</Fragment>
 		))}
 	</>;
