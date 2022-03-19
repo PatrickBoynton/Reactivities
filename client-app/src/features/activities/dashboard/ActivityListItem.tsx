02dfc82690925e1f5907c01a1058fb4264f2141c
PatrickBoynton <jpboynton3@gmail.com>
2022-03-19 15:43:46 -0400

    Removed the unused bits of code.

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,6 @@
-import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/Activity";
-import { useStore } from "../../../app/stores/store";
 
 
 interface Props {
@@ -10,14 +8,6 @@ interface Props {
 }
 
 const ActivityListItem = ({activity}: Props) => {
-	const {activityStore} = useStore();
-	const {deleteActivity, loading} = activityStore;
-	const [target, setTarget] = useState("");
-
-	const handleDeleteActivity = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
-		setTarget(e["currentTarget"].name);
-		deleteActivity(id);
-	};
 	return <Segment.Group>
 		<Segment>
 			<Item.Group>
