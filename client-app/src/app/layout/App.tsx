4761736d204d1847b09e5dc9c9a815438cf8ec71
PatrickBoynton <jpboynton3@gmail.com>
2022-03-20 16:04:37 -0400

    Added an error link, and toast container.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,35 +4,37 @@ import { Container } from "semantic-ui-react";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import TestErrors from "../../features/errors/TestErrors";
 import HomePage from "../../features/home/HomePage";
 import Navbar from "./Navbar";
+import { ToastContainer } from "react-toastify";
 
 
 const App = () => {
 	const location = useLocation();
 
-	return (
-		<>
-			<Route exact path="/" component={HomePage}/>
-			<Route
-				path={"/(.+)"}
-				render={() =>
+	return <>
+		<ToastContainer position="bottom-right" hideProgressBar />
+		<Route exact path="/" component={HomePage} />
+		<Route
+			path={"/(.+)"}
+			render={() =>
 				// noinspection BadExpressionStatementJS
-					<>
-						<Navbar/>
-						<Container style={{marginTop: "7em"}}>
-							<Route exact path="/" component={HomePage}/>
-							<Route exact path="/activities" component={ActivityDashboard}/>
-							<Route path="/activities/:id" component={ActivityDetails}/>
-							<Route key={location.key} path={["/createActivity", "/manage/:id"]}
-								component={ActivityForm}/>
-						</Container>
-					</>
-				}
-			/>
-
-		</>
-	);
+				<>
+					<Navbar />
+					<Container style={{ marginTop: "7em" }} >
+						<Route exact path="/" component={HomePage} />
+						<Route exact path="/activities" component={ActivityDashboard} />
+						<Route path="/activities/:id" component={ActivityDetails} />
+						<Route key={location.key}
+							   path={["/createActivity", "/manage/:id"]}
+							   component={ActivityForm} />
+						<Route path="/errors" component={TestErrors} />
+					</Container >
+				</>
+			}
+		/>
+	</>;
 };
 
 export default observer(App);
