1b1eca3b4aee4f650d3ed73b1a4ba763881ed43b
PatrickBoynton <jpboynton3@gmail.com>
2022-03-20 16:04:59 -0400

    Added an error testing component.

--- client-app/src/app/layout/Navbar.tsx
+++ client-app/src/app/layout/Navbar.tsx
@@ -4,17 +4,18 @@ import { Button, Container, Menu } from "semantic-ui-react";
 
 const Navbar = () => {
 
-	return <Menu inverted fixed="top">
-		<Container>
-			<Menu.Item as={NavLink} to="/" header exact>
-				<img src={"/assets/logo.png"} alt="logo" style={{marginRight: 10}}/>
-			</Menu.Item>
-			<Menu.Item as={NavLink} to="/activities" name="Activities"/>
-			<Menu.Item>
-				<Button as={NavLink} to="/createActivity" positive content="Create Activity"/>
-			</Menu.Item>
-		</Container>
-	</Menu>;
+	return <Menu inverted fixed="top" >
+		<Container >
+			<Menu.Item as={NavLink} to="/" header exact >
+				<img src={"/assets/logo.png"} alt="logo" style={{ marginRight: 10 }} />
+			</Menu.Item >
+			<Menu.Item as={NavLink} to="/activities" name="Activities" />
+			<Menu.Item as={NavLink} to="/errors" name="Errors" />
+			<Menu.Item >
+				<Button as={NavLink} to="/createActivity" positive content="Create Activity" />
+			</Menu.Item >
+		</Container >
+	</Menu >;
 };
 
 export default observer(Navbar);
