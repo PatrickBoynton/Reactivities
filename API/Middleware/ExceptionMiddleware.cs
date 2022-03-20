00910e58d82aea33fdc7e0efb297c6d24b6121ad
PatrickBoynton <jpboynton3@gmail.com>
2022-03-20 16:02:54 -0400

    Set up exception handling.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+using System.Net;
+using System.Text.Json;
+using Application.Core;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        readonly IHostEnvironment _env;
+        readonly ILogger<ExceptionMiddleware> _logger;
+        readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, exception.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new AppException(context.Response.StatusCode, exception.Message, exception.StackTrace)
+                    : new AppException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase,};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
