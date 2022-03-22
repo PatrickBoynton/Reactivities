b20101f1c6d635fddd8887e369cbb8bddd266b42
PatrickBoynton <jpboynton3@gmail.com>
2022-03-21 21:48:43 -0400

    Formatted the file.

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,17 +1,17 @@
-import {Link} from "react-router-dom";
-import {Button, Container, Header, Image, Segment} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 const HomePage = () => {
-    return <Segment inverted textAlign="center" vertical className="masthead">
-        <Container text>
-            <Header as="h1" className="homepage-header">
-                <Image size="massive" source="/assets/logo.png" alt="logo" style={{marginBottom: 12}}/>
+	return <Segment inverted textAlign="center" vertical className="masthead">
+		<Container text>
+			<Header as="h1" className="homepage-header">
+				<Image size="massive" source="/assets/logo.png" alt="logo" style={{marginBottom: 12}} />
                 Reactivities
-            </Header>
-            <Header as="h2" inverted content="Welcome to Reactivities"/>
-            <Button as={Link} to="/activities" size="huge" inverted content="Take me to the activities."/>
-        </Container>
-    </Segment>;
+			</Header>
+			<Header as="h2" inverted content="Welcome to Reactivities" />
+			<Button as={Link} to="/activities" size="huge" inverted content="Take me to the activities." />
+		</Container>
+	</Segment>;
 };
 
 
