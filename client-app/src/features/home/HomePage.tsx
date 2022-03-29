5b267b7f1d903191210826f73a680bf892141224
PatrickBoynton <jpboynton3@gmail.com>
2022-03-28 21:53:04 -0400

    Fixed an error or two.

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
@@ -5,14 +6,14 @@ const HomePage = () => {
 	return <Segment inverted textAlign="center" vertical className="masthead">
 		<Container text>
 			<Header as="h1" className="homepage-header">
-				<Image size="massive" source="/assets/logo.png" alt="logo" style={{marginBottom: 12}} />
+				<Image size="massive" src="/assets/logo.png" alt="logo" style={{marginBottom: 12}} />
                 Reactivities
 			</Header>
 			<Header as="h2" inverted content="Welcome to Reactivities" />
-			<Button as={Link} to="/activities" size="huge" inverted content="Take me to the activities." />
+			<Button as={Link} to="/activities" size="huge" inverted exact="exact">Take me to the activities.</Button>
 		</Container>
 	</Segment>;
 };
 
 
-export default HomePage;
+export default observer(HomePage);
