ee8400f03a1795fc999096f459a3d729202ac4d8
PatrickBoynton <jpboynton3@gmail.com>
2022-04-06 21:57:53 -0400

    Added some test users.

--- API/Program.cs
+++ API/Program.cs
@@ -1,40 +1,40 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            IHost host = CreateHostBuilder(args).Build();
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
 
-            using IServiceScope scope = host.Services.CreateScope();
+			using var scope = host.Services.CreateScope();
 
-            IServiceProvider services = scope.ServiceProvider;
+			var services = scope.ServiceProvider;
 
-            try
-            {
-                var context = services.GetRequiredService<DataContext>();
-                
-                await context.Database.MigrateAsync();
-                
-                await Seed.SeedData(context);
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occured during migration.");
-            }
-            
-            await host.RunAsync();
-        }
+			try
+			{
+				var context = services.GetRequiredService<DataContext>();
+				var userManager = services.GetRequiredService<UserManager<AppUser>>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+				await context.Database.MigrateAsync();
+
+				await Seed.SeedData(context, userManager);
+			}
+			catch (Exception ex)
+			{
+				var logger = services.GetRequiredService<ILogger<Program>>();
+				logger.LogError(ex, "An error occured during migration.");
+			}
+
+			await host.RunAsync();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+	}
 }
