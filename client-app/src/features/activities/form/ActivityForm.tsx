e9f79a1b4afd7529aaa67323e13da377abb164b7
PatrickBoynton <jpboynton3@gmail.com>
2022-04-06 21:26:12 -0400

    Readded the handle submit method, and hooked it up to the form.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,8 @@ import { Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Segment } from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
+import { v4 as uuid } from "uuid";
 import * as Yup from "yup";
 import CustomDatePicker from "../../../app/common/form/CustomDatePicker";
 import CustomSelectInput from "../../../app/common/form/CustomSelectInput";
@@ -12,11 +13,12 @@ import { categoryOptions } from "../../../app/common/options/categoryOptions";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import { Activity } from "../../../app/models/Activity";
 import { useStore } from "../../../app/stores/store";
+import { history } from "../../../index";
 
 
 const ActivityForm = () => {
 	const { activityStore } = useStore();
-	const { loadActivity, loading, loadingInitial } = activityStore;
+	const { loadActivity, loading, loadingInitial, createActivity, updateActivity } = activityStore;
 	const { id } = useParams<{ id: string }>();
 	const [activity, setActivity] = useState<Activity>({
 		id: "",
@@ -32,7 +34,7 @@ const ActivityForm = () => {
 		title: Yup.string().required(),
 		description: Yup.string().required(),
 		category: Yup.string().required(),
-		date: Yup.string().required(),
+		date: Yup.string().required("Date is required.").nullable(),
 		venue: Yup.string().required(),
 		city: Yup.string().required(),
 	});
@@ -42,14 +44,28 @@ const ActivityForm = () => {
 		if (id) loadActivity(id).then(activity => setActivity(activity));
 	}, [id, loadActivity]);
 
+	const handleFormSubmit = (activity: Activity) => {
+		if (activity.id.length === 0) {
+			const newActivity = {
+				...activity,
+				id: uuid(),
+			};
+			createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+		} else {
+			updateActivity(activity).then(() => history.push(`/activity/${activity.id}`));
+		}
+	};
+
+
 	if (loadingInitial) return <LoadingComponent content="Loading..." />;
 
 	return <Segment clearing>
+		<Header content="Activity Details" subheader color="teal" />
 		<Formik validationSchema={validationSchema}
 			enableReinitialize
 			initialValues={activity}
-			onSubmit={values => console.log(values)}>
-			{({ handleSubmit }) =>
+			onSubmit={values => handleFormSubmit(values)}>
+			{({ handleSubmit, isValid, isSubmitting, dirty }) =>
 				<Form className="ui form"
 					  onSubmit={handleSubmit}
 					  autoComplete="off">
@@ -62,10 +78,20 @@ const ActivityForm = () => {
 									  timeCaption="time"
 									  dateFormat="MMMM d, yyyy h:mm aa"
 					/>
+					<Header content="Location Details" sub color="teal" />
 					<CustomTextInput placeholder="City" name="city" />
 					<CustomTextInput placeholder="Venue" name="venue" />
-					<Button loading={loading} floated="right" positive type="submit" content="Submit" />
-					<Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
+					<Button disabled={isSubmitting || !dirty || !isValid}
+						loading={loading}
+						floated="right"
+						positive
+						type="submit"
+						content="Submit" />
+					<Button as={Link}
+						to="/activities"
+						floated="right"
+						type="button"
+						content="Cancel" />
 				</Form>
 			}
 		</Formik>
