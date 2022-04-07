1735cf8dd200ab391f2ba66064ba7fd01c339356
PatrickBoynton <jpboynton3@gmail.com>
2022-04-06 21:29:30 -0400

    Formatting.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -6,35 +6,33 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class Create
-    {
-        public class Command : IRequest<Result<Unit>>
-        {
-            public Activity Activity { get; set; }
-        }
+	public class Create
+	{
+		public class Command : IRequest<Result<Unit>>
+		{
+			public Activity Activity { get; set; }
+		}
 
 
-        public class CommandValidator : AbstractValidator<Command>
-        {
-            public CommandValidator() => RuleFor(activity => activity.Activity).SetValidator(new ActivityValidator());
-        }
+		public class CommandValidator : AbstractValidator<Command>
+		{
+			public CommandValidator() => RuleFor(activity => activity.Activity).SetValidator(new ActivityValidator());
+		}
 
-        public class Handler : IRequestHandler<Command, Result<Unit>>
-        {
-            readonly DataContext _context;
+		public class Handler : IRequestHandler<Command, Result<Unit>>
+		{
+			readonly DataContext _context;
 
-            public Handler(DataContext context) => _context = context;
+			public Handler(DataContext context) => _context = context;
 
-            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-            {
-                _context.Activities.Add(request.Activity);
+			public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+			{
+				_context.Activities.Add(request.Activity);
 
-                var result = await _context.SaveChangesAsync() > 0;
+				var result = await _context.SaveChangesAsync() > 0;
 
-                if (!result) return Result<Unit>.Failure("Failed to create activity");
-
-                return Result<Unit>.Success(Unit.Value);
-            }
-        }
-    }
+				return !result ? Result<Unit>.Failure("Failed to create activity") : Result<Unit>.Success(Unit.Value);
+			}
+		}
+	}
 }
