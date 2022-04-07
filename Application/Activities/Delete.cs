70fb14a28c83c592e7908df2fefe294de67bbec2
PatrickBoynton <jpboynton3@gmail.com>
2022-04-06 21:29:16 -0400

    Formatting.

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -4,32 +4,32 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class Delete
-    {
-        public class Command : IRequest<Result<Unit>>
-        {
-            public Guid Id { get; set; }
-        }
+	public class Delete
+	{
+		public class Command : IRequest<Result<Unit>>
+		{
+			public Guid Id { get; set; }
+		}
 
-        public class Handler : IRequestHandler<Command, Result<Unit>>
-        {
-            readonly DataContext _context;
+		public class Handler : IRequestHandler<Command, Result<Unit>>
+		{
+			readonly DataContext _context;
 
-            public Handler(DataContext context) => _context = context;
+			public Handler(DataContext context) => _context = context;
 
-            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
-            {
-                var activity = await _context.Activities.FindAsync(request.Id);
+			public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+			{
+				var activity = await _context.Activities.FindAsync(request.Id);
 
-                if (activity == null) return null!;
-                _context.Remove(activity!);
+				if (activity == null) return null!;
+				_context.Remove(activity);
 
-                var result = await _context.SaveChangesAsync() > 0;
+				var result = await _context.SaveChangesAsync() > 0;
 
-                if (!result) return Result<Unit>.Failure("Failed to delete the activity");
+				if (!result) return Result<Unit>.Failure("Failed to delete the activity");
 
-                return Result<Unit>.Success(Unit.Value);
-            }
-        }
-    }
+				return Result<Unit>.Success(Unit.Value);
+			}
+		}
+	}
 }
