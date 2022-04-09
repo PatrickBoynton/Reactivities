1e2aac644f7a3e934804ca3cc54e7150a4765e22
PatrickBoynton <jpboynton3@gmail.com>
2022-04-09 11:42:43 -0400

    Added authentication annotations.

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
 	public class RegisterDto
 	{
-		public string DisplayName { get; set; }
-		public string Email { get; set; }
+		[Required] public string DisplayName { get; set; }
+
+		[Required] [EmailAddress] public string Email { get; set; }
+
+		[Required]
+		[RegularExpression("(?=.*).{4,}$", ErrorMessage = "Password must contain at least 4 characters.")]
+
 		public string Password { get; set; }
-		public string Username { get; set; }
+
+		[Required] public string Username { get; set; }
 	}
 }
