6f0a710b201ef717a6a72f92b86f940689958067
PatrickBoynton <jpboynton3@gmail.com>
2022-04-09 13:40:07 -0400

    Changed the button to a login button.

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -6,11 +6,11 @@ const HomePage = () => {
 	return <Segment inverted textAlign="center" vertical className="masthead">
 		<Container text>
 			<Header as="h1" className="homepage-header">
-				<Image size="massive" src="/assets/logo.png" alt="logo" style={{marginBottom: 12}} />
-                Reactivities
+				<Image size="massive" src="/assets/logo.png" alt="logo" style={{ marginBottom: 12 }} />
+				Reactivities
 			</Header>
 			<Header as="h2" inverted content="Welcome to Reactivities" />
-			<Button as={Link} to="/activities" size="huge" inverted exact="exact">Take me to the activities.</Button>
+			<Button as={Link} to="/login" size="huge" inverted exact="exact">Login</Button>
 		</Container>
 	</Segment>;
 };
