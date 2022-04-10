2f362e689be30c01173a42d7d3e1641098ee6d9a
PatrickBoynton <jpboynton3@gmail.com>
2022-04-10 17:25:42 -0400

    Added the ability to display the proper error message on login.

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,14 +1,21 @@
-import { Form, Formik } from "formik";
+import { ErrorMessage, Form, Formik } from "formik";
 import { observer } from "mobx-react-lite";
-import { Button } from "semantic-ui-react";
+import { Button, Label } from "semantic-ui-react";
 import CustomTextInput from "../../app/common/form/CustomTextInput";
 import { useStore } from "../../app/stores/store";
 
 const LoginForm = () => {
 	const { userStore } = useStore();
-	return <Formik initialValues={{ email: "", password: "" }}
-				   onSubmit={values => userStore.login(values)}>
-		{({ handleSubmit, isSubmitting }) => <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+	return <Formik initialValues={{ email: "", password: "", error: null }}
+				   onSubmit={(values, { setErrors }) => userStore.login(values)
+					   .catch(error => setErrors({ error: "Invalid email or password." }))}>
+		{({ handleSubmit, isSubmitting, errors }) => <Form className="ui form" onSubmit={handleSubmit}
+														   autoComplete="off">
+			<ErrorMessage name="error"
+						  render={() => <Label style={{ marginBottom: 10 }}
+											   basic
+											   color="red"
+											   content={errors.error} />} />
 			<CustomTextInput placeholder="Email" name="email" />
 			<CustomTextInput placeholder="Password" name="password" type="password" />
 			<Button loading={isSubmitting} positive content="Login" fluid type="submit" />
