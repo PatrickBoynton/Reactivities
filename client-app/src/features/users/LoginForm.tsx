dc4dc000cb40d031ad6e1d781a2063e7c0b8cc7e
PatrickBoynton <jpboynton3@gmail.com>
2022-04-10 17:15:00 -0400

    Added the ability to log the login.

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,17 +1,20 @@
 import { Form, Formik } from "formik";
+import { observer } from "mobx-react-lite";
 import { Button } from "semantic-ui-react";
 import CustomTextInput from "../../app/common/form/CustomTextInput";
+import { useStore } from "../../app/stores/store";
 
 const LoginForm = () => {
+	const { userStore } = useStore();
 	return <Formik initialValues={{ email: "", password: "" }}
-				   onSubmit={values => console.log(values)}>
-		{({ handleSubmit }) => <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+				   onSubmit={values => userStore.login(values)}>
+		{({ handleSubmit, isSubmitting }) => <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
 			<CustomTextInput placeholder="Email" name="email" />
 			<CustomTextInput placeholder="Password" name="password" type="password" />
-			<Button positive content="Login" fluid type="submit" />
+			<Button loading={isSubmitting} positive content="Login" fluid type="submit" />
 		</Form>}
 
 	</Formik>;
 };
 
-export default LoginForm;
+export default observer(LoginForm);
