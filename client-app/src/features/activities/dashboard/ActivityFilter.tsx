a68c3407286edbd93908719e42e2bcd557dd581e
PatrickBoynton <jpboynton3@gmail.com>
2022-04-17 12:18:05 -0400

    Fixed a small bug.

--- client-app/src/features/activities/dashboard/ActivityFilter.tsx
+++ client-app/src/features/activities/dashboard/ActivityFilter.tsx
@@ -3,12 +3,12 @@ import { Header, Menu } from "semantic-ui-react";
 
 const ActivityFilter = () => {
 	return <>
-		<Menu vertical size="large" style={{width: "100%", marginTop: 29}}>
+		<Menu vertical size="large" style={{ width: "100%", marginTop: 29 }}>
 			<Header icon="filter" attached color="teal" content="Filters" />
 			<Menu.Item content="All activities" />
 			<Menu.Item content="I'm going" />
 			<Menu.Item content="I'm hosting" />
-		</Menu>;
+		</Menu>
 		<Header />
 		<Calendar />
 
